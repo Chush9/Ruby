684631aa6f513e47a1159dd342a058191ad8df28
Chush9 <Kuchma901991@mail.ru>
2019-04-02 13:44:58 +0300

    Update 01.rb

--- 01.rb
+++ 01.rb
@@ -4,4 +4,4 @@ puts "And what's your last name, man?"
 lname = gets.chomp
 puts "So. where are your from?"
 place = gets.chomp
-puts "Hello, " + fname +" " + lname + " from " + place + "!" 
+puts "Hello, " + fname + " " + lname + " from " + place + "!" 
