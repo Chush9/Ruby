87eacd0a6ceebf795beb4d0bb5e42fe7655cb13b
Chush9 <49187135+Chush9@users.noreply.github.com>
2019-04-09 11:15:48 +0300

    Delete 01.rb

--- 01.rb
+++
@@ -1,7 +0,0 @@
-puts "What's your firts name, man?"
-fname = gets.chomp
-puts "And what's your last name, man?"
-lname = gets.chomp
-puts "So. where are your from?"
-place = gets.chomp
-puts "Hello, " + fname + " " + lname + " from " + place + "!" 
