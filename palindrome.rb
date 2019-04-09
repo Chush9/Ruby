5228cc1101daf63c94b160edb30cb8f0fd941af6
Chush9 <49187135+Chush9@users.noreply.github.com>
2019-04-09 11:13:10 +0300

    Delete palindrome.rb

--- palindrome.rb
+++
@@ -1,9 +0,0 @@
-def palindrome?(string)
-	string = string.to_s.downcase.scan(/\w/)
- string == string.reverse
-end
-puts palindrome?("A man, a plan, a canal -- Panama")
-puts palindrome?("Madam, I'm Adam!")
-puts palindrome?(333)
-puts palindrome?(nil)
-puts palindrome?("Abracadabra") 
