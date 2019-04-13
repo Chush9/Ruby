dd3fb216e6c90b763b3ea1bac3b8cb60703b2385
Chush9 <Kuchma901991@mail.ru>
2019-04-13 21:36:46 +0300

    Update 01_table.rb

--- 01_table.rb
+++ 01_table.rb
@@ -1,8 +1,8 @@
 LW = 45
 LQ = 9
-gl1 = "Glava 1:"
-gl2 = "Glava 2:"
-gl3 = "Glava 3:"
+gl1 = "Chapter 1:"
+gl2 = "Chapter 2:"
+gl3 = "Chapter 3:"
 n1 = "Numbers "
 n2 = "Words   "
 n3 = "Variables"
@@ -10,6 +10,6 @@ p1 = "page 1"
 p2 = "page 5"
 p3 = "page 10"
 
-puts gl1.ljust(LQ) + n1.center(LQ) + p1.rjust(LW/3)
-puts gl2.ljust(LQ) + n2.center(LQ) + p2.rjust(LW/3)
-puts gl3.ljust(LQ) + n3.center(LQ) + p3.rjust(LW/3)
+puts gl1.ljust(LW/4) + n1.center(LQ) + p1.rjust(LW/2)
+puts gl2.ljust(LW/4) + n2.center(LQ) + p2.rjust(LW/2)
+puts gl3.ljust(LW/4) + n3.center(LQ) + p3.rjust(LW/2)
