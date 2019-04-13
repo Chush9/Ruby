1b5e490ae5835e3fed235bcac6878beb7cae9c16
Chush9 <Kuchma901991@mail.ru>
2019-04-13 21:27:47 +0300

    Update 01_table.rb

--- 01_table.rb
+++ 01_table.rb
@@ -1,3 +1,4 @@
+LW = 45
 LQ = 9
 gl1 = "Glava 1:"
 gl2 = "Glava 2:"
@@ -9,6 +10,6 @@ p1 = "page 1"
 p2 = "page 5"
 p3 = "page 10"
 
-puts gl1.ljust(LQ) + n1.center(LQ) + p1.rjust(LQ)
-puts gl2.ljust(LQ) + n2.center(LQ) + p2.rjust(LQ)
-puts gl3.ljust(LQ) + n3.center(LQ) + p3.rjust(LQ)
+puts gl1.ljust(LQ) + n1.center(LQ) + p1.rjust(LW/3)
+puts gl2.ljust(LQ) + n2.center(LQ) + p2.rjust(LW/3)
+puts gl3.ljust(LQ) + n3.center(LQ) + p3.rjust(LW/3)
